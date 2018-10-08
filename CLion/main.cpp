9c1583702a0a235279722ba756260b2a1ecc5e0c
eulersexception <bahadir.suezer@outlook.com>
2018-10-08 13:10:44 +0200

    all three algorithms to calculate maximum subsequence sum

--- CLion/main.cpp
+++ CLion/main.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
 int main() {
 
-    int array[] = {1, 2, -30, 4, 5};
-    int maxSum = 0;
+    int array[] = {5, -17, 12, 5, -10, 6, 4, 8, -5, -10, -17, 22, 1};
+    std::vector<int> vec1
+
 
     int length = sizeof(array) / sizeof(array[0]);
 
-    cout << "length of array " << length << endl;
-
+    // cubic complexity
+    int maxSumCubic = 0;
     for(int i = 0; i < length; i++) {
         for(int j = i; j < length; j++) {
             int thisSum = 0;
             for(int k = i; k <= j; k++) {
                 thisSum += array[k];
-                if(thisSum > maxSum)
-                    maxSum = thisSum;
+                if(thisSum > maxSumCubic)
+                    maxSumCubic = thisSum;
             }
         }
     }
-    cout << maxSum << endl;
+
+    // quadratic complexity
+    int maxSumQuadratic = 0;
+    for(int i = 0; i < length; i++) {
+        int thisSum = 0;
+        for(int j = i; j < length; j++) {
+            thisSum += array[j];
+            if(thisSum > maxSumQuadratic)
+                maxSumQuadratic = thisSum;
+        }
+    }
+
+    // linear complexity
+    int maxSumLinear = 0;
+    int thisSumLinear = 0;
+    for(int i = 0; i < length; i++) {
+        thisSumLinear += array[i];
+        if(thisSumLinear > maxSumLinear)
+            maxSumLinear = thisSumLinear;
+        else if(thisSumLinear < 0)
+            thisSumLinear = 0;
+    }
 
     return 0;
 
